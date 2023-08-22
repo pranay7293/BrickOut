3ecfb695cd60cb9e9d72b5a5f20442fbc6849db6
Pranay Anumandla <pranay7293@gmail.com>
2023-08-22 13:59:20 +0530

    BallnShooter
    Ball & Shooter Mechanics completed.

--- Assets/Scripts/BallShooter.cs
+++ Assets/Scripts/BallShooter.cs
@@ -1,67 +1,110 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallShooter : MonoBehaviour
 {
 
     [SerializeField] private LayerMask layerMask;
-    [SerializeField] Vector2 minMaxAnglel;
-    [SerializeField] GameObject ballPrefab;
-    [SerializeField] float force;
-    [SerializeField] int ballCount;
-    [SerializeField] float resetDelay = 3.0f; // Adjust this as needed
+    [SerializeField] private Vector2 minMaxAnglel;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private float force;
+    [SerializeField] private int ballCount;
+    [SerializeField] private float resetDelay = 3.0f; 
+    [SerializeField] private float speed;
 
 
     private int currentBallCount;
     private int ballsReturned;
+    private bool hasBallsReturned;
+    private bool isShooting = false;
+    private bool decreasedHeight;
+
     private Vector3 initialPosition;
     private RaycastHit2D ray;
     private float angle;
 
-
-    public void FixedUpdate()
+    private void Start()
     {
-        if (Input.GetMouseButton(0))
+        ballsReturned = 0;
+        hasBallsReturned = true;
+        decreasedHeight = true;
+        currentBallCount = ballCount;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (!isShooting)
         {
-            ray = Physics2D.Raycast(transform.position, transform.up, 20.0f, layerMask);
-            //Debug.DrawRay(transform.position, ray.point, Color.red);
-
-            Vector2 reflectPos = Vector2.Reflect(new Vector3(ray.point.x, ray.point.y) - transform.position, ray.normal);
-            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 dir = Input.mousePosition - pos;
-
-            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
-
-            if (angle >= minMaxAnglel.x && angle <= minMaxAnglel.y)
-            {
-                DotsLine.instance.DrawDottedLine(transform.position, ray.point);
-                DotsLine.instance.DrawDottedLine(ray.point, ray.point + reflectPos.normalized * 2f);
-                transform.rotation = Quaternion.AngleAxis(angle, transform.forward);
-            }
-
+            Move(horizontal);
         }
 
-
-    }
-    public void Update()
-    {
-        if (Input.GetMouseButtonUp(0))
+        if (ballsReturned == currentBallCount)
         {
+            Time.timeScale = 1.0f;
+            decreasedHeight = false;
+            hasBallsReturned = true;
+            ballsReturned = 0;
+        }
+
+        if (hasBallsReturned && !decreasedHeight)
+        {
+            decreasedHeight = true;
+            //levelController.MoveBricks();
+        }
+
+        if (Input.GetMouseButton(0) && !isShooting)
+        {
+            AimToShoot();
+        }
+
+        if (Input.GetMouseButtonUp(0) && !isShooting && hasBallsReturned)
+        {
+            isShooting = true;
+            hasBallsReturned = false;
             StartCoroutine(ShootBalls());
         }
     }
+
+    private void AimToShoot()
+    {
+        ray = Physics2D.Raycast(transform.position, transform.up, 20.0f, layerMask);
+
+        Vector2 reflectPos = Vector2.Reflect(new Vector3(ray.point.x, ray.point.y) - transform.position, ray.normal);
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+        if (angle >= minMaxAnglel.x && angle <= minMaxAnglel.y)
+        {
+            DotsLine.instance.DrawDottedLine(transform.position, ray.point);
+            DotsLine.instance.DrawDottedLine(ray.point, ray.point + reflectPos.normalized * 2f);
+            transform.rotation = Quaternion.AngleAxis(angle, transform.forward);
+        }
+    }
+
+    private void Move(float _horizontal)
+    {
+        Vector3 temp = transform.position;
+        temp.x = Mathf.Clamp(temp.x + speed * _horizontal * Time.deltaTime, -5, 5);
+        transform.position = temp;
+    }
+    
     public IEnumerator ShootBalls()
     {
+        ballsReturned = 0;
         for (int i = 0; i < ballCount; i++)
         {
+            currentBallCount--;
             yield return new WaitForSeconds(0.2f);
             GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
             ball.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
         }
-        // Wait for the specified delay, then reset the position
-        yield return new WaitForSeconds(resetDelay);
-        ResetShooterPosition();
+        isShooting = false;
     }
     private void ResetShooterPosition()
     {
--- Assets/Scripts/DotsLine.cs
+++ Assets/Scripts/DotsLine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DotsLine : MonoBehaviour
 {
+   
+
     //Static Property with backing field
     public static DotsLine instance;
     public void Awake()
@@ -24,56 +26,65 @@ public class DotsLine : MonoBehaviour
     public float alpha;
 
     //Utility fields
-    List<Vector2> positions = new List<Vector2> ();
-    List<GameObject> dots = new List<GameObject> ();
+    List<Vector2> positions = new List<Vector2>();
+    List<GameObject> dots = new List<GameObject>();
 
     // Update is called once per frame
-    void FixedUpdate () {
-        if(positions.Count > 0) {
-            DestroyAllDots ();
-            positions.Clear ();
+    void FixedUpdate()
+    {
+        if (positions.Count > 0)
+        {
+            DestroyAllDots();
+            positions.Clear();
         }
 
     }
 
-    private void DestroyAllDots () {
-        foreach(var dot in dots) {
-            Destroy (dot);
+    private void DestroyAllDots()
+    {
+        foreach (var dot in dots)
+        {
+            Destroy(dot);
         }
-        dots.Clear ();
+        dots.Clear();
     }
 
-    GameObject GetOneDot () {
-        var gameObject = new GameObject ();
+    GameObject GetOneDot()
+    {
+        var gameObject = new GameObject();
         gameObject.transform.localScale = Vector3.one * Size;
         gameObject.transform.parent = transform;
 
-        var sr = gameObject.AddComponent<SpriteRenderer> ();
-        sr.color = new Color (1, 1, 1, alpha);
+        var sr = gameObject.AddComponent<SpriteRenderer>();
+        sr.color = new Color(1, 1, 1, alpha);
         sr.sprite = Dot;
 
         return gameObject;
     }
 
-    public void DrawDottedLine (Vector2 start, Vector2 end) {
-        DestroyAllDots ();
+    public void DrawDottedLine(Vector2 start, Vector2 end)
+    {
+        DestroyAllDots();
 
         Vector2 point = start;
-        Vector2 direction = ( end - start ).normalized;
+        Vector2 direction = (end - start).normalized;
 
-        while(( end - start ).magnitude > ( point - start ).magnitude) {
-            positions.Add (point);
-            point += ( direction * Delta );
+        while ((end - start).magnitude > (point - start).magnitude)
+        {
+            positions.Add(point);
+            point += (direction * Delta);
         }
 
-        Render ();
+        Render();
     }
 
-    private void Render () {
-        foreach(var position in positions) {
-            var g = GetOneDot ();
+    private void Render()
+    {
+        foreach (var position in positions)
+        {
+            var g = GetOneDot();
             g.transform.position = position;
-            dots.Add (g);
+            dots.Add(g);
         }
     }
 }
